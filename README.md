f838893ee779ce52fcfc40c357aa85cc539b3336
Carlos J Vargas E <carlosvargasmc@gmail.com>
2022-10-05 17:14:59 -0500

    first cjvesla

---
+++ README.md
@@ -0,0 +1,5 @@
+# cjvesla
+
+![cjvesla8](https://user-images.githubusercontent.com/104727028/192542747-c8b1095d-3abc-4db4-bcf0-56d7ea56b0c6.PNG)
+
+https://cjvesla.web.app
---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import carReducer from "../features/car/carSlice"
+import counterReducer from '../features/counter/counterSlice';
+export const store = configureStore({
+  reducer: {
+    car: carReducer
+    //counter: counterReducer,
+  },
+});
